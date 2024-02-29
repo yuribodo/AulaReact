dacb93f69b81ad735abdfffbb379c4c7a6a07123
yuribodo <yemotalara@gmail.com>
2024-02-29 05:52:34 -0400

    'commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+
+const Hero = () => {
+  return (
+    <div className='text-white'>
+        <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
+            <p className='text-[#00df9a] font-bold p-2'>
+                Growing with data analytics
+            </p>
+            <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>Grow with Data.</h1>
+            <div>
+                <p className='md:text-5xl sm:text-4xl text-xl font-bold'>
+                    Fast, flexible, financing for
+                </p>
+
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
